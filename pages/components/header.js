94b9e67ee991602d126c72d8c724ebace366284b
Rıdvan Karadağ <ridvan.karadag@std.izu.edu.tr>
2018-02-27 23:04:19 +0300

    Header componenti oluşturuldu
    Sadece Text döndüren basit bir component oluşturuldu

---
+++ pages/components/header.js
@@ -0,0 +1,11 @@
+// Import Libraries
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make Component
+const Header = () => {
+    return <Text>Albums</Text>;
+};
+
+// Make exportable
+export default Header;
