889efa69d111e63529a6636600c8c8d56aec8a00
Rıdvan Karadağ <ridvan.karadag@std.izu.edu.tr>
2018-02-28 23:05:37 +0300

    [Styling 101] Header componentine ilk stil verildi

--- pages/components/header.js
+++ pages/components/header.js
@@ -4,7 +4,14 @@ import { Text } from 'react-native';
 
 // Make Component
 const Header = () => {
-    return <Text>Albums</Text>;
+    const { textStyle } = styles;
+    return <Text style={textStyle}>Albums</Text>;
+};
+
+const styles = {
+ textStyle: {
+     fontSize: 20
+ }
 };
 
 // Make exportable
