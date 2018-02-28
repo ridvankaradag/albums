38ad0e46fd4f6b5ffd7dab223db1331fca870351
Rıdvan Karadağ <ridvan.karadag@std.izu.edu.tr>
2018-02-28 23:27:26 +0300

    App.js gereksiz kod bloğu kaldırıldı

--- App.js
+++ App.js
@@ -24,37 +24,10 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Header />
-        <Text style={styles.welcome}>
-          2018 First React Native App
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
