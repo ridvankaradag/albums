e1dc731e11b56f39807e335dcf89632557742e03
Rıdvan Karadağ <ridvan.karadag@std.izu.edu.tr>
2018-02-28 23:46:30 +0300

    Component için boilerplate oluşturuldu

---
+++ pages/components/ComponentStarter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AlbumList = () => {
+    const {  } = styles;
+    return (
+        <View>
+        <Text>Album List!</Text>
+        </View>
+    )
+};
+
+const styles = {
+
+};
+
+// Make exportable
+export default AlbumList;
