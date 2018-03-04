81d1cc309fe3ecf5ed653fc9b5bcba14d44eac3c
Rıdvan Karadağ <ridvan.karadag@std.izu.edu.tr>
2018-03-04 14:44:37 +0300

    AlbumDetail componenti oluşturuldu
    Yalnızca veri gösterecek, function based component hazırlandı

---
+++ pages/components/AlbumDetail.js
@@ -0,0 +1,12 @@
+import React from 'React';
+import { View, Text } from 'react-native';
+
+const AlbumDetail = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    ) 
+};
+
+export default AlbumDetail;
