c9a4809709560d1ee647df9362931082f2505049
Rıdvan Karadağ <ridvan.karadag@std.izu.edu.tr>
2018-03-04 17:35:50 +0300

    Scroll sorunu çözüldü

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const instructions = Platform.select({
 export default class App extends Component {
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <Header headerText={'Albums'}/>
         <AlbumList />
       </View>
