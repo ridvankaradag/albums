c6d82b79b2c99171660343902a9807c04aef5a15
Rıdvan Karadağ <ridvan.karadag@std.izu.edu.tr>
2018-03-04 17:19:45 +0300

    Card gövde kısmı oluşturuldu

--- pages/components/AlbumDetail.js
+++ pages/components/AlbumDetail.js
@@ -5,20 +5,26 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, image } = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
+    const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image 
                         style={thumbnailStyle} 
                         source={{ uri: image }}
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+            <CardSection>
+                    <Image 
+                        style={imageStyle} 
+                        source={{ uri: image }}
+                    />
             </CardSection>
 
         </Card>
@@ -30,9 +36,24 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 }
 
